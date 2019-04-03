fcb276e7c5b8e7a21e0d7490eba82ef570d0e577
Michael <michael@adl-crm.uk>
2019-04-03 11:20:15 +0100

    Setup project for Assignment 7: Time to Practice - Component Communication

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
     <div class="container">
+        <app-header></app-header>
+        <hr>
         <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
-            </div>
+            <servers></servers>
+            <app-server-details></app-server-details>
         </div>
+        <hr>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
     export default {
         components: {
-            appUser: User
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
         }
     }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+
 </style>

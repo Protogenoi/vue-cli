a231d40021a52d444c8cff96cac12c85a0f9e8f7
Michael <michael@adl-crm.uk>
2019-04-03 14:19:32 +0100

    Updated for section 9: Advanced component usage.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default content</p>
+                    </component>
+                </keep-alive>
+                <!--                <app-quote>
+                                    <h2 slot="title">{{ quoteTitle }}</h2>
+                                    <p>A wonderful quote!</p>
+                                </app-quote>-->
             </div>
         </div>
     </div>
@@ -10,8 +23,27 @@
 
 <script>
 
-    export default {}
+    import Quote from './components/Quote'
+    import Author from './components/Author'
+    import New from './components/New'
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+
+
+    }
 </script>
 
 <style>
+
 </style>

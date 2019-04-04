5944631cc19d34cee44c71d59e88622e6a4abb17
Michael <michael@adl-crm.uk>
2019-04-04 13:51:26 +0100

    Finished section 10: Wonderful Quotes.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
     <div class="container">
-
+        <app-header :quote-count="quotes.length" :max-quotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on quote to delete it.</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {}
+
+    import QuoteGrid from './components/QuotesGrid'
+    import NewQuote from './components/NewQuote'
+    import Header from './components/Header'
+
+    export default {
+        data: function () {
+            return {
+                quotes: [
+                    'Just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first!');
+                }
+                this.quotes.push(quote)
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
 </script>
 
 <style>

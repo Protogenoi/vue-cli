44671b7fe0a940aa24caf8d9f89304b12735abdc
Michael <michael@adl-crm.uk>
2019-04-08 13:22:37 +0100

    Added starting code for section 13: Improving your App with Filters and Mixins. Moved section 12 code to its own folder

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <button v-myOn:click="greet">Touch me</button>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <h1>Filters & Mixins</h1>
 
             </div>
         </div>
@@ -14,23 +11,9 @@
 
 <script>
     export default {
-        directives: {
-            'myOn': {
-                bind(el, binding, vnode) {
-
-                    el.addEventListener(binding.arg, binding.value);
-
-                }
-
-            }
-        },
-        methods: {
-            greet() {
-                alert('Hey!')
-            }
-        }
     }
 </script>
 
 <style>
+
 </style>

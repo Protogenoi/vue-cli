3db449183691133827d399a5b946cd1830ae1bb6
Michael <michael@adl-crm.uk>
2019-04-08 13:19:03 +0100

    Finished code for Assignment 10: Time to Practice - Directives

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
+                <button v-myOn:click="greet">Touch me</button>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,7 +13,23 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            'myOn': {
+                bind(el, binding, vnode) {
+
+                    el.addEventListener(binding.arg, binding.value);
+
+                }
+
+            }
+        },
+        methods: {
+            greet() {
+                alert('Hey!')
+            }
+        }
+    }
 </script>
 
 <style>

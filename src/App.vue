92bcef3bfa943a701f235e424599a92f1fa9bbaf
Michael <michael@adl-crm.uk>
2019-04-09 16:32:51 +0100

    Added section 14: Adding Animations and Transitions starting code.

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <!-- Exercise 1) -->
-                <!-- Build a local Filter which reverses the Text it is applied on -->
-                <p>{{ text | reverseText }}</p>
-                <!-- Exercise 2 -->
-                <!-- Build a global Filter which counts the length of a word and it appends it -->
-                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                <hr>
-                <p>{{ countThis | get-Length }}</p>
-                <!-- Exercise 3 -->
-                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-                <hr>
-                <p>Count: {{ countStr }} | Reverse: {{ reverseStr }}</p>
-                <!-- Exercise 4 -->
-                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+                <h1>Animations</h1>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {stringMixin} from './stringsMixin'
-
     export default {
-        mixins: [stringMixin],
         data() {
-            return {
-                text: 'Reverse this!',
-                countThis: 'Count this string'
-            }
-        },
-        filters: {
-            reverseText(value) {
-                return value.split('').reverse().join('');
-            }
-        },
-        components: {
-            appStringMixin: stringMixin
+            return {}
         }
     }
 </script>
 
 <style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('get-Length', function (value) {
-    return value + ' (' + value.length + ')';
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)

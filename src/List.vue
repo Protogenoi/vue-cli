fd8fdda6ac8bf4b2042711f5cc7b6fa9915ad736
Michael <michael@adl-crm.uk>
2019-04-09 13:34:31 +0100

    Ending code for section 13: Improving your App with Filters and Mixins.

---
+++ src/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+    import {fruitMixin} from './fruitMixin';
+
+    export default {
+        mixins: [fruitMixin],
+        created() {
+            console.log('Inside list created!')
+        }
+
+    }
+</script>
+
+<style>
+</style>

d5aa9d7195a092e4554b16442548f8b40b56ce10
Protogenoi <michael@adl-crm.uk>
2019-04-11 17:13:44 +0100

    Added starting code for section 16: Routing in a VueJS Application

--- src/main.js
+++ src/main.js
@@ -1,19 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource);
-
-Vue.http.options.root = 'https://vuejs-http-bb93d.firebaseio.com/';
-Vue.http.interceptors.push((request, next) => {
-  console.log(request);
-  if (request.method == 'POST') {
-    request.method = 'PUT';
-  }
-  next(response => {
-    response.json = () => { return {messages: response.body} }
-  });
-});
 
 new Vue({
   el: '#app',

1bb5066c976a75d7fa7dcea7cfe52b63499e6513
Michael <michael@adl-crm.uk>
2019-04-15 13:51:40 +0100

    Finished section 16: Routing in a VueJS Application

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,7 +4,33 @@
         <p>User loaded has ID: {{ $route.params.id }}</p>
         <router-link
                 tag="button"
-                :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+                :to="link"
                 class="btn btn-primary">Edit user</router-link>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en', q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        },
+        beforeRouteEnter(to, form, next) {
+            if (true) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,48 @@
-import User from './components/user/User'
-import UserStart from './components/user/UserStart'
-import UserDetail from './components/user/UserDetail'
-import UserEdit from './components/user/UserEdit'
 import Home from './components/Home'
+import Header from './components/Header'
+
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User'));
+    }, 'user');
+};
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart'));
+    }, 'user');
+};
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail'));
+    }, 'user');
+};
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit'));
+    }, 'user');
+};
 
 export const routes = [
-    { path: '', component:  Home, name: 'home' },
-    { path: '/user', component:  User, children: [
+    {
+        path: '', name: 'home', components: {
+            default: Home,
+            'header-top': Header
+        }
+    },
+    {
+        path: '/user', components: {
+            default: User,
+            'header-bottom': Header
+        }, children: [
             { path: '', component: UserStart },
-            { path: ':id', component: UserDetail },
+            {
+                path: ':id', component: UserDetail, beforeEnter: (to, from, next) => {
+                    console.log('Inside route setup');
+                    next();
+                }
+            },
             { path: ':id/edit', component: UserEdit, name: 'userEdit' },
         ] },
+    {path: '/redirect-me', redirect: 'home'},
+    {path: '*', redirect: '/'}
 ];

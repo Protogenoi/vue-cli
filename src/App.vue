f11e648f8735b97098ac80b069fde1d0769f059c
Michael <michael@adl-crm.uk>
2019-04-15 13:54:54 +0100

    Added starting code for section 17: Better State Management with Vuex

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,29 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Routing</h1>
+                <h1>Vuex</h1>
+                <app-result :counter="counter"></app-result>
                 <hr>
-                <router-view name="header-top"></router-view>
-                <transition name="slide" mode="out-in">
-                    <router-view></router-view>
-                </transition>
-                <router-view name="header-bottom"></router-view>
+                <app-counter @updated="counter += $event"></app-counter>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Header from './components/Header'
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
+
     export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
         components: {
-            appHeader: Header
+            appCounter: Counter,
+            appResult: Result,
         }
     }
 </script>
 
-<style>
-    .slide-leave-active {
-        transition: opacity 1s ease;
-        opacity: 0;
-        animation: slide-out 1s ease-out forwards;
-    }
-
-    .slide-leave {
-        opacity: 1;
-        transform: translateX(0);
-    }
-
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-    }
-
-    @keyframes slide-out {
-        0% {
-            transform: translateY(0);
-        }
-        100% {
-            transform: translateY(-30px);
-        }
-    }
-
-    @keyframes slide-in {
-        0% {
-            transform: translateY(-30px);
-        }
-        100% {
-            transform: translateY(0);
-        }
-    }
-</style>

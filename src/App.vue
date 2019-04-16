b34a44e53bc363d45a7e2dc3ba7bde32288210ef
Michael <michael@adl-crm.uk>
2019-04-16 17:13:10 +0100

    Updated section 19: Final Project - The Stock Trader. 290. Adding some Order Checks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
 
+            </div>
+        </div>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 
-    export default {}
+    import Header from './components/Header'
+
+    export default {
+        components: {
+            AppHeader: Header
+        },
+        created() {
+            this.$store.dispatch('initStocks');
+        }
+    }
 </script>
 
 <style>
-
+    body {
+        padding: 30px;
+    }
 </style>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+
+    import {mapGetters} from 'vuex';
+    import Stock from './Stock';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stocksPortfolio'
+            })
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>

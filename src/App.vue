b1217a6f9e173cdffcfbdc6f7b240fdafadfe1e6
Michael <michael@adl-crm.uk>
2019-04-17 12:16:00 +0100

    Finished section 19: Final Project - The Stock Trader.

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
         <app-header></app-header>
         <div class="row">
             <div class="col-xs-12">
-
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
-        <router-view></router-view>
+
     </div>
 </template>
 
@@ -27,5 +29,39 @@
 <style>
     body {
         padding: 30px;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource';
+
 import App from './App.vue'
 import {routes} from './routes'
 import store from './store/store'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-d4803.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '£' + value.toLocaleString();
+});
 
 const router = new VueRouter({
   routes,

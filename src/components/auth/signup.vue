a53bf9926dcf654d7909704e9ef41ff503d5e9cd
Protogenoi <michael@adl-crm.uk>
2019-04-19 16:38:41 +0100

    Finished section 24: Form Input Validation with vuelidate

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,32 +2,39 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
+                  @blur="$v.email.$touch()"
                   v-model="email">
+          <p v-if="!$v.email.email">Please provide a valid email address.</p>
+          <p v-if="!$v.email.required">This field must not be empty.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
+                  @blur="$v.age.$touch()"
                   v-model.number="age">
+          <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.password.$error}">
           <label for="password">Password</label>
           <input
                   type="password"
                   id="password"
+                  @blur="$v.password.$touch()"
                   v-model="password">
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
           <input
                   type="password"
                   id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
                   v-model="confirmPassword">
         </div>
         <div class="input">
@@ -39,6 +46,7 @@
             <option value="germany">Germany</option>
           </select>
         </div>
+
         <div class="hobbies">
           <h3>Add some Hobbies</h3>
           <button @click="onAddHobby" type="button">Add Hobby</button>
@@ -46,22 +54,32 @@
             <div
                     class="input"
                     v-for="(hobbyInput, index) in hobbyInputs"
+                    :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                     :key="hobbyInput.id">
               <label :for="hobbyInput.id">Hobby #{{ index }}</label>
               <input
                       type="text"
                       :id="hobbyInput.id"
+                      @blur="$v.hobbyInputs.$each[index].value.$touch()"
                       v-model="hobbyInput.value">
               <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
             </div>
+            <p v-if="!$v.hobbyInputs.minLen">You have to specify at least {{ $v.hobbyInputs.$params.minLen.min }} hobbies.</p>
+            <p v-if="!$v.hobbyInputs.required">Please add hobbies.</p>
           </div>
         </div>
-        <div class="input inline">
-          <input type="checkbox" id="terms" v-model="terms">
+
+        <div class="input inline" :class="{invalid: $v.terms.$invalid}">
+          <input
+                  type="checkbox"
+                  id="terms"
+                  @change="$v.terms.$touch()"
+                  v-model="terms">
           <label for="terms">Accept Terms of Use</label>
         </div>
+
         <div class="submit">
-          <button type="submit">Submit</button>
+          <button type="submit" :disabled="$v.$invalid">Submit</button>
         </div>
       </form>
     </div>
@@ -69,6 +87,10 @@
 </template>
 
 <script>
+
+  import { required, email, numeric, minValue, minLength, sameAs, requiredUnless} from 'vuelidate/lib/validators'
+
+  import axios from 'axios'
 
   export default {
     data () {
@@ -79,7 +101,52 @@
         confirmPassword: '',
         country: 'usa',
         hobbyInputs: [],
-        terms: false
+        terms: ''
+      }
+    },
+    validations: {
+      email: {
+        required,
+        email,
+        unique: val => {
+          if (val === '') return true;
+            return axios.get('/user.json?orderBy="email"&equalTo="' + val + '"')
+                    .then(res => {
+                      return Object.keys(res.data).length === 0;
+                    })
+        }
+      },
+      age: {
+        required,
+        numeric,
+        minVal: minValue(18)
+
+      },
+      password: {
+        required,
+        minLen: minLength(4)
+      },
+      confirmPassword: {
+        sameAs: sameAs('password')
+/*        sameAs: sameAs(vm => {
+          return vm.password + 'b'
+        })*/
+      },
+      terms: {
+        //sameAs: sameAs( () => true ),
+        required: requiredUnless(vm => {
+          return vm.country === 'germany'
+        })
+      },
+      hobbyInputs: {
+        required,
+        minLen: minLength(2),
+        $each: {
+          value: {
+            required,
+            minLen: minLength(5)
+          }
+        }
       }
     },
     methods: {
@@ -151,6 +218,15 @@
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+  }
+
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: #ff8480;
+  }
+
+  .input.invalid label {
+    color: red;
   }
 
   .input select {
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vuelidate from 'vuelidate'
 import axios from 'axios'
 
 import router from './router'
 import store from './store'
 
+Vue.use(Vuelidate);
 
 axios.defaults.baseURL = 'https://vs-js-axios.firebaseio.com';
 // axios.defaults.headers.common['Authorization'] = 'asdasds';
@@ -29,4 +30,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-})
+});
